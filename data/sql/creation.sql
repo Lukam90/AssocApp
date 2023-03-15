71178849eb6bbad090d618e141ad4b8e3c67c128
Lukam90 <lucien.hamm@gmail.com>
2023-03-15 23:04:28 +0100

    MAJ Nv Valeurs Test > SQL

--- data/sql/creation.sql
+++ data/sql/creation.sql
@@ -17,9 +17,8 @@ CREATE TABLE IF NOT EXISTS mode
     label VARCHAR(50)
 );
 
-INSERT INTO mode (label) VALUES ('Espèces');
-INSERT INTO mode (label) VALUES ('Chèque');
-INSERT INTO mode (label) VALUES ('Carte Bancaire');
+INSERT INTO mode (label) VALUES
+('Espèces'), ('Chèque'), ('Carte Bancaire');
 
 -- Les newsletters (newsletter)
 
@@ -33,12 +32,10 @@ CREATE TABLE IF NOT EXISTS newsletter
     send_at DATE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO newsletter (object, target, content, is_send, send_at)
-VALUES ('Annonce du prochain salon', 'Général', 'Lorem ipsum...', 1, '2023-03-15');
-INSERT INTO newsletter (object, target, content, is_send, send_at)
-VALUES ('Prochaine assemblée générale', 'Membres', 'Lorem ipsum...', 0, '2023-06-16');
-INSERT INTO newsletter (object, target, content, is_send, send_at)
-VALUES ('Repas de Noël du comité', 'Membres', 'Lorem ipsum...', 1, '2023-11-25');
+INSERT INTO newsletter (object, target, content, is_send, send_at) VALUES 
+('Annonce du prochain salon', 'Général', 'Lorem ipsum...', 1, '2023-03-15'),
+('Prochaine assemblée générale', 'Membres', 'Lorem ipsum...', 0, '2023-06-16'),
+('Repas de Noël du comité', 'Membres', 'Lorem ipsum...', 1, '2023-11-25');
 
 -- Les utilisateurs (user)
 
@@ -57,20 +54,34 @@ CREATE TABLE IF NOT EXISTS `user`
     role VARCHAR(20) NOT NULL DEFAULT 'Exposant'
 );
 
---INSERT INTO user ()
+INSERT INTO user (email, password, role, first_name, last_name, label) VALUES
+('contact@admin.fr', 'password', 'Administrateur', 'Lucien', 'HAMM', 'AACCP'),
+('reservations@admin.fr', 'password', 'Trésorier', 'Jean', 'DUPONT', ''),
+('l.roman@test.com', 'password', 'Exposant', 'Louis', 'ROMAN', 'Louis La Brocante'),
+('g.delajungle@test.com', 'password', 'Exposant', 'Georges', 'DELAJUNGLE', ''),
+('b.duval@test.com', 'password', 'Exposant', 'Bernadette', 'DUVAL', 'Association Cartophile de Haguenau'),
+('f.molas@jdg.fr', 'password', 'Exposant', 'Frédéric', 'MOLAS', 'Joueur du Grenier'),
+('d.goodenough@jdg.fr', 'password', 'Exposant', 'David', 'GOODENOUGH', ''),
+('p.richard@test.com', 'password', 'Exposant', 'Pierre', 'RICHARD', 'La Boutique de Pierrot'),
+('g.bonnet@test.com', 'password', 'Exposant', 'Gilbert', 'BONNET', 'Gilbert Philatélie'),
+('j.bond@test.com', 'password', 'Exposant', 'James', 'BOND', 'Tableaux 007'),
+('m.simon@test.com', 'password', 'Exposant', 'Marc', 'SIMON', 'Cartes Postales Magazine');
 
 -- Les événements (event)
 
 CREATE TABLE IF NOT EXISTS `event`
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    planned_at DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
+    planned_at DATE NOT NULL,
+    content TEXT NOT NULL,
     picture BLOB,
-    content TEXT,
-    min_price DECIMAL(3,2),
+    min_price DECIMAL(5,2),
     num_available INT(3)
 );
+
+INSERT INTO event (title, planned_at, content, min_price, num_available) VALUES 
+('43ème Foire Alsacienne de la Carte Postale et du Vieux Papier de Collection et Salon des Collectionneurs', '2023-05-07', 'Lorem ipsum...', 30.00, 300);
 
 -- Les réservations (reservation)
 
@@ -86,18 +97,35 @@ CREATE TABLE IF NOT EXISTS reservation
     FOREIGN KEY (event_id) REFERENCES event (id)
 );
 
+INSERT INTO reservation (status, paid_at, comments, event_id, mode_id) VALUES
+('Payé', '2023-03-01', 'une table offerte (membre)', 1, 1),
+('Payé', '2023-03-02', '', 1, 2),
+('A Payer', '2023-03-10', '', 1, 3),
+('Annulé', '2023-03-03', '', 1, 1),
+('Payé', '2023-03-04', '', 1, 2),
+('Payé', '2023-03-05', '', 1, 3);
+
 -- Les tables (table)
 
 CREATE TABLE IF NOT EXISTS `table`
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    price DECIMAL(3,2) NOT NULL DEFAULT 0,
+    price DECIMAL(5,2) NOT NULL DEFAULT 0,
     pos_x INT(3) NOT NULL DEFAULT 0,
     pos_y INT(3) NOT NULL DEFAULT 0,
     comments TEXT,
     reservation_id INT NOT NULL,
     FOREIGN KEY (reservation_id) REFERENCES reservation (id)
 );
+
+INSERT INTO `table` (reservation_id, price, pos_x, pos_y, comments) VALUES
+(1, 30, 0, 0, ''),
+(1, 30, 0, 1, ''),
+(1, 30, 1, 0, ''),
+(1, 30, 1, 1, 'près de la buvette'),
+(2, 30, 1, 2, 'près de la portie de sortie'),
+(2, 30, 2, 1, ''),
+(2, 0, 2, 2, 'table offerte (membre)');
 
 -- La table intermédiaire newsletter_user
 
@@ -109,6 +137,13 @@ CREATE TABLE IF NOT EXISTS newsletter_user
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
+INSERT INTO newsletter_user (newsletter_id, user_id) VALUES
+(1,1), (1,2), (1,4),
+(1,5), (1,6), (1,7),
+(1,9), (1,10), (1,11),
+(2,1), (2,4),
+(2,5), (2,6);
+
 -- La table intermédiaire user_event
 
 CREATE TABLE IF NOT EXISTS user_event
@@ -119,58 +154,7 @@ CREATE TABLE IF NOT EXISTS user_event
     FOREIGN KEY (event_id) REFERENCES event (id)
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO user_event (event_id, user_id) VALUES
+(1,1), (1,2), (1,3), (1,4), 
+(1,5), (1,6), (1,7), (1,8), 
+(1,9), (1,10), (1,11);
