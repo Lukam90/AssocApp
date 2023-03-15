f08e97ae0bcf278963586d684fc00f9df04e3e78
Lukam90 <lucien.hamm@gmail.com>
2023-03-15 22:01:03 +0100

    MAJ Nv Ajouts Newsletter

--- data/sql/creation.sql
+++ data/sql/creation.sql
@@ -1,33 +1,50 @@
--- Les modes de paiement (mode)
-
+DROP TABLE IF EXISTS `table`;
+DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS mode;
+
+DROP TABLE IF EXISTS `event`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS newsletter;
+
+DROP TABLE IF EXISTS newsletter_user;
+DROP TABLE IF EXISTS user_event;
+
+-- Les modes de paiement (mode)
 
 CREATE TABLE IF NOT EXISTS mode
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     label VARCHAR(50)
 );
 
--- Les newsletters (newsletter)
+INSERT INTO mode (label) VALUES ('Espèces');
+INSERT INTO mode (label) VALUES ('Chèque');
+INSERT INTO mode (label) VALUES ('Carte Bancaire');
 
-DROP TABLE IF EXISTS newsletter;
+-- Les newsletters (newsletter)
 
 CREATE TABLE IF NOT EXISTS newsletter
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    object VARCHAR(255),
     target VARCHAR(10) NOT NULL DEFAULT 'Général',
     content VARCHAR(255),
     is_send BOOLEAN NOT NULL DEFAULT 0,
     send_at DATE NOT NULL DEFAULT NOW()
 );
 
--- Les utilisateurs (user)
+INSERT INTO newsletter (object, target, content, is_send, send_at)
+VALUES ('Annonce du prochain salon', 'Général', 'Lorem ipsum...', 1, '2023-03-15');
+INSERT INTO newsletter (object, target, content, is_send, send_at)
+VALUES ('Prochaine assemblée générale', 'Membres', 'Lorem ipsum...', 0, '2023-06-16');
+INSERT INTO newsletter (object, target, content, is_send, send_at)
+VALUES ('Repas de Noël du comité', 'Membres', 'Lorem ipsum...', 1, '2023-11-25');
 
-DROP TABLE IF EXISTS `user`;
+-- Les utilisateurs (user)
 
 CREATE TABLE IF NOT EXISTS `user`
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
@@ -40,13 +57,13 @@ CREATE TABLE IF NOT EXISTS `user`
     role VARCHAR(20) NOT NULL DEFAULT 'Exposant'
 );
 
--- Les événements (event)
+--INSERT INTO user ()
 
-DROP TABLE IF EXISTS `event`;
+-- Les événements (event)
 
 CREATE TABLE IF NOT EXISTS `event`
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     planned_at DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
     picture BLOB,
@@ -57,48 +74,50 @@ CREATE TABLE IF NOT EXISTS `event`
 
 -- Les réservations (reservation)
 
-DROP TABLE IF EXISTS reservation;
-
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     status VARCHAR(20) NOT NULL DEFAULT 'A Payer',
     paid_at DATE,
     comments TEXT,
-    mode_id INT,
-    event_id INT,
+    mode_id INT NOT NULL,
+    event_id INT NOT NULL,
     FOREIGN KEY (mode_id) REFERENCES mode (id),
     FOREIGN KEY (event_id) REFERENCES event (id)
 );
 
 -- Les tables (table)
 
-DROP TABLE IF EXISTS `table`;
-
 CREATE TABLE IF NOT EXISTS `table`
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     price DECIMAL(3,2) NOT NULL DEFAULT 0,
     pos_x INT(3) NOT NULL DEFAULT 0,
     pos_y INT(3) NOT NULL DEFAULT 0,
     comments TEXT,
-    reservation_id INT,
+    reservation_id INT NOT NULL,
     FOREIGN KEY (reservation_id) REFERENCES reservation (id)
 );
 
+-- La table intermédiaire newsletter_user
 
+CREATE TABLE IF NOT EXISTS newsletter_user
+(
+    newsletter_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (newsletter_id) REFERENCES newsletter (id),
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
 
+-- La table intermédiaire user_event
 
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE IF NOT EXISTS user_event
+(
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (event_id) REFERENCES event (id)
+);
 
 
 
