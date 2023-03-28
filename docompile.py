9fff9cdd5cf73bf8d29cca88b59ed5af54f00c46
Lukam90 <lucien.hamm@gmail.com>
2023-03-28 18:28:06 +0200

    MAJ Nv Division Script

--- docompile.py
+++ docompile.py
@@ -1,132 +1,18 @@
 #coding: utf8
 
-import re
+from docscript import document, add_part
 
-from docx import Document
-from docx.shared import Cm
 
-# Doc Table
 
-def add_table(filename, num_cols):
-    global document
+# Set Font & Style
 
-    table = document.add_table(rows = 1, cols = num_cols)
-    table.style = "Table Grid"
-
-    with open(f"docs/tables/{filename}", "r") as file:
-        for line in file:
-            line = line.strip()
-
-            cols = line.split(";")
-
-            row_cells = table.add_row().cells
-
-            for index in range(0, num_cols):
-                col = cols[index]
-                row = row_cells[index]
-
-                row.text = col
-
-    file.close()
-
-# Doc Image
-
-def add_image(filename):
-    global document
-
-    document.add_picture(f"docs/{filename}", width = Cm(15))
-
-# Doc Textual Content
-
-def add_content(line, style = "Normal"):
-    global document
-
-    if "*" in line:
-        words = line.split(" ")
-
-        p = document.add_paragraph(style = style)
-
-        for word in words:
-            word += " "
-
-            if word[0:2] == "**":
-                word = word.replace("**", "")
-
-                p.add_run(word).bold = True
-            elif word[0] == "*":
-                word = word.replace("*", "")
-
-                p.add_run(word).italic = True
-            else:
-                p.add_run(word)
-    else:
-        document.add_paragraph(line, style = style)
-
-# Doc Part
-
-def add_part(filename):
-    global document
-
-    with open(f"docs/parts/{filename}", "r") as file:
-        for line in file:
-            line = line.strip()
-
-            if line != "":
-                first = line[0]
-
-                if first == "#":
-                    parts = line.split(" ")
-
-                    level = 1
-
-                    if line[0:2] == "##": level = 2
-
-                    line = re.sub("#\s?", "", line)
-
-                    document.add_heading(line, level)
-
-                    document.add_paragraph()
-                elif first == "-":
-                    line = line.replace("- ", "")
-
-                    add_content(line, style = "List Bullet")
-                elif first == "%":
-                    line = line.replace("%", "")
-
-                    parts = line.split(";")
-
-                    filename = parts[0]
-                    num_cols = int(parts[1])
-
-                    add_table(filename, num_cols)
-
-                    document.add_paragraph()
-                elif first == "*":
-                    line = line.replace("*", "")
-
-                    p = document.add_paragraph()
-                    p.add_run(line).bold = True
-                elif first == "!":
-                    filename = line[1:].replace("\"", "")
-                    
-                    add_image(filename)
-                else:
-                    add_content(line)
-
-        document.add_page_break()
-
-        file.close()
-
-# Docx file
-document = Document()
-
-# Set font
 style = document.styles["Normal"]
-font = style.font
 
+font = style.font
 font.name = "Calibri"
 
 # Add parts
+
 #files = ["EN-resume.md", "context-projet.md"]
 
 #add_part("_test.md")
---
+++ docscript.py
@@ -0,0 +1,137 @@
+#coding: utf8
+
+import re
+
+from docx import Document
+from docx.shared import Cm
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+# Docx File
+
+document = Document()
+
+# Doc Table
+
+def add_table(filename, num_cols):
+    global document
+
+    table = document.add_table(rows = 1, cols = num_cols)
+    table.style = "Table Grid"
+
+    with open(f"docs/tables/{filename}", "r") as file:
+        for line in file:
+            line = line.strip()
+
+            cols = line.split(";")
+
+            row_cells = table.add_row().cells
+
+            for index in range(0, num_cols):
+                col = cols[index]
+                row = row_cells[index]
+
+                row.text = col
+
+    file.close()
+
+# Doc Image
+
+def add_image(filename):
+    global document
+
+    document.add_picture(f"docs/{filename}", width = Cm(15))
+
+# Doc Textual Content
+
+def add_content(line, style = "Normal"):
+    global document
+
+    if "*" in line:
+        words = line.split(" ")
+
+        p = document.add_paragraph(style = style)
+
+        for word in words:
+            word += " "
+
+            if word[0:2] == "**":
+                word = word.replace("**", "")
+
+                p.add_run(word).bold = True
+            elif word[0] == "*":
+                word = word.replace("*", "")
+
+                p.add_run(word).italic = True
+            else:
+                p.add_run(word)
+    else:
+        p = document.add_paragraph(line, style = style)
+    
+    p.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# Doc Part
+
+def add_part(filename):
+    global document
+
+    with open(f"docs/parts/{filename}", "r") as file:
+        for line in file:
+            line = line.strip()
+
+            if line != "":
+                first = line[0]
+
+                if first == "#":
+                    parts = line.split(" ")
+
+                    level = 1
+
+                    if line[0:2] == "##": level = 2
+
+                    line = re.sub("#\s?", "", line)
+
+                    document.add_heading(line, level)
+
+                    document.add_paragraph()
+                elif first == "-":
+                    line = line.replace("- ", "")
+
+                    add_content(line, style = "List Bullet")
+                elif first == "%":
+                    line = line.replace("%", "")
+
+                    parts = line.split(";")
+
+                    filename = parts[0]
+                    num_cols = int(parts[1])
+
+                    add_table(filename, num_cols)
+
+                    document.add_paragraph()
+                elif first == "*":
+                    line = line.replace("*", "")
+
+                    p = document.add_paragraph()
+                    p.add_run(line).bold = True
+                elif first == "!":
+                    filename = line[1:].replace("\"", "")
+                    
+                    add_image(filename)
+                else:
+                    add_content(line)
+
+        document.add_page_break()
+
+        file.close()
+
+# Compile Parts
+
+def compile():
+    with open("docs/parts.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+
+            if len(line) > 0 and line[0] != "#":
+                filename = line + ".md"
+
+        file.close()
